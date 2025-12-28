232b0740c95eb321527d06fa17e474adcbca5b6b
turulomio <turulomio@yahoo.es>
2025-12-28 08:38:02 +0100

    save_url-1.1.0. Updated dependencies for security reasons (#11)
    * Added tag in .gitignore
    
    * Updated dependencies
    
    * save_url-1.1.0

--- save_url/__init__.py
+++ save_url/__init__.py
@@ -1,4 +1,4 @@
 from datetime import datetime
-__version__="1.0.0"
-__versiondatetime__=datetime(2025, 4, 13, 12, 56)
+__version__="1.1.0"
+__versiondatetime__=datetime(2025, 12, 28, 8, 35)
 __versiondate__=__versiondatetime__.date()
