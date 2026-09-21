594a054a2a9b6513f7e82504fa4ff16020914d5e
turulomio <turulomio@yahoo.es>
2026-09-21 10:40:35 +0200

    save_url-1.2.0 (#24)

--- save_url/__init__.py
+++ save_url/__init__.py
@@ -1,4 +1,4 @@
 from datetime import datetime
-__version__="1.1.0"
-__versiondatetime__=datetime(2025, 12, 28, 8, 35)
+__version__="1.2.0"
+__versiondatetime__=datetime(2026, 9, 21, 10, 36)
 __versiondate__=__versiondatetime__.date()
